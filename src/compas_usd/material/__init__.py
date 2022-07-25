b86a3b1e8890b163e2fa565efb875e88f7b455bb
Romana Rust <rust@arch.ethz.ch>
2022-07-25 10:53:37 +0200

    Update __init__.py

--- src/compas_usd/material/__init__.py
+++ src/compas_usd/material/__init__.py
@@ -1,1 +1,19 @@
-from .material import *
+"""
+********************************************************************************
+compas_usd.material
+********************************************************************************
+
+.. currentmodule:: compas_usd.material
+
+.. toctree::
+    :maxdepth: 2
+"""
+from __future__ import absolute_import
+
+from .material import USDMaterial
+from .material import USDPreviewSurface
+
+__all__ = [
+    "USDMaterial",
+    "USDPreviewSurface",
+]
