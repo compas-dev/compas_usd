7d892cb8364e50ce3490d544124111e55e56d0d5
Romana Rust <rust@arch.ethz.ch>
2022-07-25 10:53:13 +0200

    black

--- src/compas_usd/conversions/__init__.py
+++ src/compas_usd/conversions/__init__.py
@@ -10,34 +10,27 @@ compas_usd.conversions
 """
 from __future__ import absolute_import
 
-from .geometry import (
-    prim_from_box,
-    prim_from_cylinder,
-    prim_from_sphere,
-    prim_from_mesh,
-    prim_from_transformation
-
-)
+from .geometry import prim_from_box, prim_from_cylinder, prim_from_sphere, prim_from_mesh, prim_from_transformation, prim_default
 from .transformations import (
     gfmatrix4d_from_transformation,
     transformation_from_gfmatrix4d,
     gfvec3f_and_gfquatd_from_frame,
     xform_rotate_from_frame,
     apply_transformation_on_prim,
-    apply_rotate_and_translate_on_prim
+    apply_rotate_and_translate_on_prim,
 )
 
 __all__ = [
-    'prim_from_box',
-    'prim_from_cylinder',
-    'prim_from_sphere',
-    'prim_from_mesh',
-    'prim_from_transformation',
-
-    'gfmatrix4d_from_transformation',
-    'transformation_from_gfmatrix4d',
-    'gfvec3f_and_gfquatd_from_frame',
-    'xform_rotate_from_frame',
-    'apply_transformation_on_prim',
-    'apply_rotate_and_translate_on_prim',
+    "prim_from_box",
+    "prim_from_cylinder",
+    "prim_from_sphere",
+    "prim_from_mesh",
+    "prim_from_transformation",
+    "prim_default",
+    "gfmatrix4d_from_transformation",
+    "transformation_from_gfmatrix4d",
+    "gfvec3f_and_gfquatd_from_frame",
+    "xform_rotate_from_frame",
+    "apply_transformation_on_prim",
+    "apply_rotate_and_translate_on_prim",
 ]
