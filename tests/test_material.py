a298c57f7a612daaa0a21f11d6495309be948ef4
Romana Rust <rust@arch.ethz.ch>
2022-07-26 07:40:19 +0200

    Update test_material.py

--- tests/test_material.py
+++ tests/test_material.py
@@ -1,6 +1,8 @@
 import os
 import pytest
 
+from pxr import Usd
+
 from compas_usd.material import USDMaterial
 from compas_usd.material import USDPreviewSurface
 
@@ -17,16 +19,13 @@ def grey_mdl():
     return os.path.join(BASE_FOLDER, "fixtures", "grey.mdl")
 
 
-def test_blue_material(blue_mdl):
-
-    from pxr import Usd
-    from compas_usd import DATA
-
-    # stage = Usd.Stage.CreateNew(os.path.join(DATA, "materials.usda"))
+def test_blue_mdl(blue_mdl):
     stage = Usd.Stage.CreateInMemory()
     USDMaterial.from_mdl(stage, blue_mdl)
+    print(stage.GetRootLayer().ExportToString())
 
-    material = USDMaterial(stage, "preview_surface")
-    preview_surface = USDPreviewSurface(stage, material)
 
+def test_grey_mdl(grey_mdl):
+    stage = Usd.Stage.CreateInMemory()
+    USDMaterial.from_mdl(stage, grey_mdl)
     print(stage.GetRootLayer().ExportToString())
